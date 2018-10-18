01ea69b0da78a1eb07790bdd91e6b4150c2fa04c
Mihir Shah <mihir.u.shah@accenture.com>
2018-10-19 02:32:32 +0800

    Test Boxever call

--- index.js
+++ index.js
@@ -2,11 +2,26 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
-
+const request = require('request-promise');
 const app = express();
-const TestResp = {
-  speech: 'spoken response (google home for instance)',
-  displayText: 'written response (google assistant or slack for instance)'
+
+const options = {
+  method: 'POST',
+  uri: 'https://api-ap-southeast-2-production.boxever.com/v2/callFlows',
+  body: {"context": 
+	{
+		"browserId": "1920c00f-b9ff-4e3e-b2d8-bcc9258ea9b6",
+		"clientKey": "scuatvAGHM9ke1RfXDVgJmE61D5HobSw",
+		"channel": "WEB",
+		"language": "EN",
+		"currencyCode": "SGD",
+		"uri": "Home Page Post Login",
+		"region": "Destinations",
+		"pointOfSale": "accentureshowcase.com"
+	}
+},
+  json: true
+  // JSON stringifies the body automatically
 };
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -18,23 +33,28 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.json())
 app.post('/fulfillment', (req, res) => {
   console.log(req.body);
-  const response = {
-    fulfillmentText: "Your webhook works fine !",
+  const Errresponse = {
+    fulfillmentText: "Your API call does not works fine !",
   }
-  res.json(response);
+
+  request(options)
+    .then(function (response) {
+      // Handle the response
+      console.log(response.result.channel);
+      let resp = {
+        fulfillmentText: response.result.offers[0].attributes.Name
+      };
+      res.json(resp);
+    })
+    .catch(function (err) {
+      // Deal with the error
+      res.json(Errresponse);
+    })
+
 
 
 });
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-app.get('/', (req, res) => res.render('pages/index'))
-app.post('/testAPI', (req, res) => res.send("TestApi"))
-app.get('/times', (req, res) => {
-  let result = ''
-  const times = process.env.TIMES || 5
-  for (i = 0; i < times; i++) {
-    result += i + ' '
-  }
-  res.send(result)
-})
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('pages/index'));
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
