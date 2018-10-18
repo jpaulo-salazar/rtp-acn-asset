7e7d8e97b9012c390c8bf48968c1074a08d2cb38
Mihir Shah <mihir.u.shah@accenture.com>
2018-10-18 16:38:25 +0800

    new project creation

---
+++ index.js
@@ -0,0 +1,21 @@
+const cool = require('cool-ascii-faces')
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/testAPI', (req, res) => res.send("TestApi"))
+  .get('/times', (req, res) => {
+    let result = ''
+    const times = process.env.TIMES || 5
+    for (i = 0; i < times; i++) {
+      result += i + ' '
+    }
+    res.send(result)
+  })
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
